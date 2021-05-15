8187611ec0682c9b31bb35984d13de5eb00ed504
FrediNans <80039008+FrediNans@users.noreply.github.com>
2021-05-15 09:18:15 +0200

    mise en place du panier

--- js/cart.js
+++ js/cart.js
@@ -1,32 +1,40 @@
-const product = sessionStorage.getItem("cart");
-const products = JSON.parse(product);
+/// Constant used to recover and parse cart in session storage ///
+const cart = sessionStorage.getItem("cart");
+const productsInCart = JSON.parse(cart);
 
+/// Function used to display each product in cart ///
 (function () {
-	for (productAdded of products) {
-		displayProductAdded();
+	for (productAdded of productsInCart) {
+		displayProductsAdded();
 	}
 })();
 
-function displayProductAdded() {
-	const cartTableCol1 = document.getElementById("cart__table__col-1");
-	const cartTableCol2 = document.getElementById("cart__table__col-2");
-	const cartTableCol3 = document.getElementById("cart__table__col-3");
-	const cartTableCol4 = document.getElementById("cart__table__col-4");
+function displayProductsAdded() {
+	const productNameColumn = document.getElementById("product__name__column");
+	const productAmountColumn = document.getElementById("amount__column");
+	const productPriceColumn = document.getElementById("price__column");
+	const productTotalPriceColumn = document.getElementById(
+		"total__product__price__column"
+	);
+	const amountCaseForInput = document.getElementById("case");
 	const productPrice = (productAdded.price / 1000).toFixed(2) + " €";
 	const productPriceTotal =
 		((productAdded.price * productAdded.amount) / 1000).toFixed(2) + " €";
 
-	let productName = document.createElement("td");
+	let productName = document.createElement("div");
 	productName.textContent = productAdded.name;
-	let amount = document.createElement("td");
-	amount.textContent = productAdded.amount;
-	let price = document.createElement("td");
+	productName.setAttribute("class", "case");
+	let amountOfProduct = document.createElement("div");
+	amountOfProduct.textContent = productAdded.amount;
+	amountOfProduct.setAttribute("id", "case");
+	let price = document.createElement("div");
 	price.textContent = productPrice;
-	let totalPrice = document.createElement("td");
-	totalPrice.textContent = productPriceTotal;
-
-	cartTableCol1.appendChild(productName);
-	cartTableCol2.appendChild(amount);
-	cartTableCol3.appendChild(price);
-	cartTableCol4.appendChild(totalPrice);
+	price.setAttribute("class", "case");
+	let totalProductPrice = document.createElement("div");
+	totalProductPrice.textContent = productPriceTotal;
+	totalProductPrice.setAttribute("class", "case");
+	productNameColumn.appendChild(productName);
+	productAmountColumn.appendChild(amountOfProduct);
+	productPriceColumn.appendChild(price);
+	productTotalPriceColumn.appendChild(totalProductPrice);
 }
