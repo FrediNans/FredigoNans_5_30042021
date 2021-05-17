c9894d6ddb6c46d4b581ba10977be34f7b564875
FrediNans <80039008+FrediNans@users.noreply.github.com>
2021-05-17 13:50:56 +0200

    test input mobile form

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,11 @@
+var timer;
+
+window.addEventListener("resize", function () {
+	clearTimeout(timer);
+	timer = setTimeout(function () {
+		window.location.reload();
+	}, 500);
+});
 /// Constant used to recover and parse cart in session storage ///
 const cart = sessionStorage.getItem("cart");
 var productsInCart = JSON.parse(cart);
@@ -27,13 +35,13 @@ function displayProductsAdded() {
 
 		let productName = document.createElement("div");
 		productName.textContent = productAdded.name;
-		productName.setAttribute("class", "box");
+		productName.setAttribute("class", "box fs-5");
 		let price = document.createElement("div");
 		price.textContent = productPrice;
-		price.setAttribute("class", "box");
+		price.setAttribute("class", "box fs-5");
 		let totalProductPrice = document.createElement("div");
 		totalProductPrice.textContent = productPriceTotal;
-		totalProductPrice.setAttribute("class", "box");
+		totalProductPrice.setAttribute("class", "box fs-5");
 
 		productNameColumn.appendChild(productName);
 		productPriceColumn.appendChild(price);
@@ -59,7 +67,7 @@ function displayAmountBox() {
 
 		let amountOfProduct = document.createElement("div");
 		amountOfProduct.setAttribute("id", "box-" + i);
-		amountOfProduct.setAttribute("class", "box d-flex");
+		amountOfProduct.setAttribute("class", "box d-flex ");
 
 		productAmountColumn.appendChild(amountOfProduct);
 	}
@@ -76,53 +84,77 @@ function displayInputs() {
 			"class",
 			"box col-6 col-md-3 col-lg-2 mx-auto border-0 text-center bg-light"
 		);
+		if (window.matchMedia("(max-width: 576px)").matches) {
+			amountInput.setAttribute("type", "number");
+			amountInput.setAttribute(
+				"class",
+				"box col-6 h-50 col-md-3 col-lg-2 mx-auto border-2 border-secondary text-center bg-white br1"
+			);
+		}
 
 		productAmountBox.appendChild(amountInput);
 	}
 }
 
 function displayButtons() {
-	for (let i = 0; i < productsInCart.length; i++) {
-		const productAmountBox = document.getElementById("box-" + i);
+	if (window.matchMedia("(min-width: 576px)").matches) {
+		for (let i = 0; i < productsInCart.length; i++) {
+			const productAmountBox = document.getElementById("box-" + i);
 
-		let moreAmountButtons = document.createElement("button");
-		moreAmountButtons.setAttribute(
-			"class",
-			"text-center col-3 h-50 btn-secondary br1 border-0 text-white fw-bold"
-		);
-		moreAmountButtons.setAttribute("id", "addButton-" + i);
-		moreAmountButtons.textContent = "+";
-		let lessAmountButtons = document.createElement("button");
-		lessAmountButtons.setAttribute(
-			"class",
-			"text-center col-3 h-50 btn-secondary br1 border-0 text-white fw-bold"
-		);
-		lessAmountButtons.setAttribute("id", "removeButton-" + i);
-		lessAmountButtons.textContent = "-";
-		productAmountBox.appendChild(moreAmountButtons);
-		productAmountBox.prepend(lessAmountButtons);
+			let moreAmountButtons = document.createElement("button");
+			moreAmountButtons.setAttribute(
+				"class",
+				"text-center col-3 h-50 btn-secondary br1 border-0 text-white fw-bold"
+			);
+			moreAmountButtons.setAttribute("id", "addButton-" + i);
+			moreAmountButtons.textContent = "+";
+			let lessAmountButtons = document.createElement("button");
+			lessAmountButtons.setAttribute(
+				"class",
+				"text-center col-3 h-50 btn-secondary br1 border-0 text-white fw-bold"
+			);
+			lessAmountButtons.setAttribute("id", "removeButton-" + i);
+			lessAmountButtons.textContent = "-";
+			productAmountBox.appendChild(moreAmountButtons);
+			productAmountBox.prepend(lessAmountButtons);
+		}
 	}
 }
 console.log(productsInCart);
 function recoverButtons() {
 	for (let i = 0; i < productsInCart.length; i++) {
-		document
-			.getElementById("addButton-" + i)
-			.addEventListener("click", function () {
-				productsInCart[i].amount++;
-				sessionStorage.setItem("cart", JSON.stringify(productsInCart));
-				window.location.reload();
-				console.log(productsInCart[i].amount);
-			});
-		document
-			.getElementById("removeButton-" + i)
-			.addEventListener("click", function () {
-				if (productsInCart[i].amount > 1) {
-					productsInCart[i].amount--;
+		if (window.matchMedia("(min-width: 576px)").matches) {
+			document
+				.getElementById("addButton-" + i)
+				.addEventListener("click", function () {
+					productsInCart[i].amount++;
 					sessionStorage.setItem("cart", JSON.stringify(productsInCart));
 					window.location.reload();
 					console.log(productsInCart[i].amount);
-				}
-			});
+				});
+			document
+				.getElementById("removeButton-" + i)
+				.addEventListener("click", function () {
+					if (productsInCart[i].amount > 1) {
+						productsInCart[i].amount--;
+						sessionStorage.setItem("cart", JSON.stringify(productsInCart));
+						window.location.reload();
+						console.log(productsInCart[i].amount);
+					}
+				});
+		}
+		if (window.matchMedia("(max-width: 576px)").matches) {
+			document
+				.getElementById("input-" + i)
+				.addEventListener("blur", function () {
+					productsInCart[i].amount = document.getElementById(
+						"input-" + i
+					).value;
+					sessionStorage.setItem("cart", JSON.stringify(productsInCart));
+					window.location.reload();
+
+					console.log(productsInCart[i].amount);
+				});
+		}
 	}
 }
